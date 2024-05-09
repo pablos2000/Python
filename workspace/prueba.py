2ac063c2241afb697f9d3dd8471bb565a7bc790b
pablos2000 <mapablosca@gmail.com>
2024-05-09 12:46:50 +0200

    new file:   workspace/estilo.css 	new file:   workspace/index.html 	new file:   workspace/prueba.py

---
+++ workspace/prueba.py
@@ -0,0 +1,8 @@
+# operador ternario
+num = 12
+var = "par" if (num % 2 == 0) else "impar"
+# seria como escribir
+num = 12
+if num % 2 == 0:
+    print="El numero introducido es Par"
+else: print ="impar"
